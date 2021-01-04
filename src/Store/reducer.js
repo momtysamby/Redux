82ab3f2102d136143fa301002383262d99030d07
momtysamby <momtysamby@gmail.com>
2021-01-04 20:59:40 +0300

    Deleting data from array immutably

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -12,6 +12,16 @@ const reducer = (state = initialState, action) => {
         case 'SUBTRACT': return {...state, counter: state.counter - action.val}
         //concat = push(), but concat does't manipulate original value. Returns new array = old array + the value
         case 'STORE_RESULT': return {...state, results: state.results.concat({id: new Date(), value: state.counter})}
+        case 'DELETE_RESULT': 
+        // One way of removing data from array immutably
+        // const id = 2
+        // const newArray = [...state.results]
+        // newArray.splice(id, 1)
+
+        //Second way
+        //Returns true for all elements where id DOESN'T equal to the id we pass (resultElId) in Counter container
+        const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+        return {...state, results: updatedArray}
     }
     return state
 }
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -20,7 +20,7 @@ class Counter extends Component {
                     {/* Map each result to a list item. Map array of results into array of JSX */}
                     {this.props.storedResults.map(strResult => (
                         <li key={strResult.id} 
-                            onClick={this.props.onDeleteResult}>{strResult.value}
+                            onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}
                         </li>
                     ))}
                 </ul>
@@ -43,7 +43,7 @@ const mapDispatchToProps = dispatch => {
         onAddCounter: () => dispatch({type: 'ADD', val: 10}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15}),
         onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     }
 }
 
