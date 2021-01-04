6c022c0a9f6967b530485ab80e11a56b212837f3
momtysamby <momtysamby@gmail.com>
2021-01-04 20:05:48 +0300

    Switch case in Reducer

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,32 +3,11 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return{
-            ...state,
-            counter: state.counter + 1
-        }
-    }
-
-    if (action.type === 'DECREMENT') {
-        return{
-            ...state,
-            counter: state.counter - 1
-        }
-    }
-
-    if (action.type === 'ADD') {
-        return{
-            ...state,
-            counter: state.counter + 10
-        }
-    }
-
-    if (action.type === 'SUBTRACT') {
-        return{
-            ...state,
-            counter: state.counter - 8
-        }
+    switch(action.type) {
+        case 'INCREMENT': return {...state, counter: state.counter + 1}
+        case 'DECREMENT': return {...state, counter: state.counter - 1}
+        case 'ADD': return {...state, counter: state.counter + action.val}
+        case 'SUBTRACT': return {...state, counter: state.counter - action.val}
     }
     return state
 }
