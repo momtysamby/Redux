d76054fa6d35e560b6d4e0f6b223d4c6d37ae754
momtysamby <momtysamby@gmail.com>
2021-01-04 19:17:01 +0300

    Dispatching logic added

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,6 +3,33 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return{
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'DECREMENT') {
+        return{
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD') {
+        return{
+            ...state,
+            counter: state.counter + 10
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return{
+            ...state,
+            counter: state.counter - 8
+        }
+    }
     return state
 }
 
