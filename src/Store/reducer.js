abacf727ac0f89ed5190818ce8c1d5f52ddca194
momtysamby <momtysamby@gmail.com>
2021-01-05 00:14:03 +0300

    Outsourcing action types

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions'
+
 const initialState = {
     counter: 0,
     results: []
@@ -6,13 +8,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         //distributing old state, overwriting the counter, not touching results. IMMUTABLY
-        case 'INCREMENT': return {...state, counter: state.counter + 1} 
-        case 'DECREMENT': return {...state, counter: state.counter - 1}
-        case 'ADD': return {...state, counter: state.counter + action.val}
-        case 'SUBTRACT': return {...state, counter: state.counter - action.val}
+        case actionTypes.INCREMENT: return {...state, counter: state.counter + 1} 
+        case actionTypes.DECREMENT: return {...state, counter: state.counter - 1}
+        case actionTypes.ADD: return {...state, counter: state.counter + action.val}
+        case actionTypes.SUBTRACT: return {...state, counter: state.counter - action.val}
         //concat = push(), but concat does't manipulate original value. Returns new array = old array + the value
-        case 'STORE_RESULT': return {...state, results: state.results.concat({id: new Date(), value: state.counter})}
-        case 'DELETE_RESULT': 
+        case actionTypes.STORE_RESULT: 
+        return {...state, results: state.results.concat({id: new Date(), value: state.counter})}
+        case actionTypes.DELETE_RESULT: 
         // One way of removing data from array immutably
         // const id = 2
         // const newArray = [...state.results]
