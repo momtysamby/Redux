b6d2416ace1e4d14e740f2c22a1cc3d6b73bd896
momtysamby <momtysamby@gmail.com>
2021-01-07 20:34:35 +0300

    Custom Middleware function added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from './Store/reducers/counter'
 import resultReducer from './Store/reducers/result'
@@ -13,7 +13,20 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res:resultReducer
 })
-const store = createStore(rootReducer)
+
+//Adding Middleware to the project
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] dispatching', action)
+            const result = next(action)
+            console.log('[Middleware] next store', store.getState())
+            return result
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
